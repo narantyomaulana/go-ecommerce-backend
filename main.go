1eece45542e077473b951a872cf6023837de04a0
Narantyo Maulana <olandeoxys@gmail.com>
2025-07-07 19:37:24 +0700

    ADD gitignore and setup database using supabase

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 	"os"
@@ -8,16 +9,22 @@ import (
 	"github.com/joho/godotenv"
 	"github.com/narantyomaulana/go-grpc-ercommerce-be/internal/handler"
 	"github.com/narantyomaulana/go-grpc-ercommerce-be/pb/service"
+	"github.com/narantyomaulana/go-grpc-ercommerce-be/pkg/database"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
 
 func main() {
+	ctx := context.Background()
+
 	godotenv.Load()
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Panicf("Error when listening: %v", err)
 	}
+
+	database.ConnectDB(ctx, os.Getenv("DB_URI"))
+	log.Println("Database connection established")
 
 	serviceHandler := handler.NewServiceHandler()
 
